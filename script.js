41812296ce3c630205658bc8871bc10dd7afb8b3
BiodunIjelu <biodunijelu@gmail.com>
2023-12-27 11:56:33 +0100

    adding financial records

---
+++ script.js
@@ -0,0 +1,11 @@
+// Sample dataset for financial records
+const financialData = [
+    ['Jan-2023', 867884],
+    ['Feb-2023', 984655],
+    ['Mar-2023', 322013],
+    ['Apr-2023', 2000],
+    ['May-2023', 1000],
+    ['June-2023', 1500],
+    ['July-2023', -500],
+  
+  ];
