cc447b01b83c88ed36b2938ebe5b1c7fbe6c537f
BiodunIjelu <biodunijelu@gmail.com>
2023-12-27 12:18:57 +0100

    To calculate financial analysis

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ const financialData = [
     ['July-2023', -500],
   
   ];
+
+  // To calculate financial analysis
+  function calculateFinancialAnalysis(data) {
+    let totalMonths = data.length;
+    let totalProfitLoss = 0;
+    let totalChange = 0;
+    let greatestIncrease = { amount: 0 };
+    let greatestDecrease = { amount: 0 };
+
+    for (let i = 0; i < totalMonths; i++) {
+      // Extracting values from the current record
+      let currentData = data[i];
+      let currentProfitLoss = parseInt(currentData[1]);
+  
+      // Calculating total Profit/Loss
+      totalProfitLoss += currentProfitLoss;
+  
+      // Calculating change in Profit/Loss
+      if (i > 0) {
+        let change = currentProfitLoss - parseInt(data[i - 1][1]);
+        totalChange += change;
+  
+        // Updating greatest increase and decrease
+        if (change > greatestIncrease.amount) {
+          greatestIncrease.amount = change;
+          greatestIncrease.date = currentData[0];
+        } else if (change < greatestDecrease.amount) {
+          greatestDecrease.amount = change;
+          greatestDecrease.date = currentData[0];
+        }
+      }
+    }
+
+
+}
